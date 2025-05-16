0a181728bf4f00a4d32649248e703659eee32d96
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-05-16 18:15:13 -0400

    added reels button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../styles/Navbar.css';
 import backgroundImage from "../assets/floralnav.png";
+import { BiMoviePlay } from 'react-icons/bi';
 
 function Navbar() {
   return (
-       <nav 
+    <nav 
       className="navbar navbar-bg"
       style={{
         '--navbar-bg': `url(${backgroundImage})`
@@ -12,14 +13,26 @@ function Navbar() {
     >
       <div className="navbar-name">Samantha Kerivan♡</div>
 
-      <a 
-        className="book-button" 
-        href="https://www.instagram.com/samantha.kerivan/" 
-        target="_blank" 
-        rel="noopener noreferrer"
-      >
-        Book with me
-      </a>
+      <div className="navbar-actions">
+        <a 
+          className="book-button" 
+          href="https://www.instagram.com/samantha.kerivan/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          Book with me
+        </a>
+
+        <a 
+          className="reels-icon" 
+          href="https://www.instagram.com/samantha.kerivan/reels/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          title="Watch Reels"
+        >
+          <BiMoviePlay size={32} />
+        </a>
+      </div>
     </nav>
   );
 }
